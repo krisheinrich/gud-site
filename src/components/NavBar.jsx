c54a5429e0842aa20f7e509ebf596b89dc8ec3a0
Kris Heinrich <krisheinrich@gmail.com>
2020-08-31 21:56:43 -0700

    Make logo link to homepage; add underline effect on hover

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
     <div className="NavBar">
       <div className="container row">
-        <div className="NavBar-logo">
-          <span className="text-green">GROUND UP</span>
-          &nbsp;
-          <span className="text-yellow">DEV</span>
-        </div>
+        <Link to="/">
+          <div className="NavBar-logo">
+            <span className="text-green">GROUND UP</span>
+            &nbsp;
+            <span className="text-yellow">DEV</span>
+          </div>
+        </Link>
         <div className="NavBar-notice">
           Early Access
         </div>
       </div>
     </div>
-  )
+  );
 }
